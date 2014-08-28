03bc38bb34a2c0ee705833cf549330d96a8bca1f
Julian Müller <julian.mueller.ffb@kabelmail.de>
2014-08-28 19:13:13 +0200

    Login Screen erstellt

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+	<head>
+		<link href="css/login.css" type="text/css" rel="stylesheet">
+		<title>Letter</title>
+	</head>
+	<body>
+		<!-- zentrieren -->
+		<div class="container">
+			<div class="middle">
+				<!-- Logo -->
+				<img width="150px" src="img/logo.png" />
+				<!-- Login-Formular -->
+				<form method="POST" action="index.php">
+					<div class="input-group">
+						<input type="text" name="email" placeholder="E-Mailadresse" maxlength="50" class="input input-first" />
+						<p class="p-group" />
+						<input type="password" name="pwd" placeholder="Passwort" maxlength="30" class="input input-last password" />
+					</div>
+					<p></p>
+					<input type="submit" class="button button-blue" value="Einloggen" />
+					<br />
+					<a href="de.de" /><input type="button" value="Registrieren" class="button button-green" /></a>
+				</form>
+				<div class="footer">
+					<a href="https://www.facebook.com/pages/Letter/668067103275242?ref=hl">Letter</a> - a modern way to write letters
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
